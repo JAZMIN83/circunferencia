194e661092a0d30001be3e371d3efae60d5aed5f
jazmin.patino <jazmin.patino@Tecnologia1>
2017-11-01 16:36:49 -0500

    mejoras del formulario Generador Mixto

---
+++ JavaApplication5/src/Circuferencia/Circuferencia.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Circuferencia;
+
+/**
+ *
+ * @author jazmin.patino
+ */
+public class Circuferencia {
+
+     public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo");
+        System.out.println("Guia de simulacion");
+        
+    }
+    
+}
