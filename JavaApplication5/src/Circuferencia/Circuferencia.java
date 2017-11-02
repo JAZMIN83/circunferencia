60421c067a8543a2bfb4bc6b343aa665656dc481
JAZMIN83 <31301942+JAZMIN83@users.noreply.github.com>
2017-11-02 16:45:35 -0500

    Update Circuferencia.java

--- JavaApplication5/src/Circuferencia/Circuferencia.java
+++ JavaApplication5/src/Circuferencia/Circuferencia.java
@@ -13,6 +13,14 @@ public class Circuferencia {
 
      public static void main(String[] args) {
         // TODO code application logic here
+            double radio, area;         
+         System.out.println("HALLA EL AREA DE UN CIRCULO");
+         System.out.print("Ingresar el radio r = ");
+         Scanner scanner = new Scanner(System.in);
+         radio = scanner.nextFloat();
+         area =  (3.14159*radio*radio);            
+         System.out.println("Area = " + area);
+          
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
